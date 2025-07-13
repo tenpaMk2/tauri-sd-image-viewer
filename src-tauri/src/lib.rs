45526ce9e1effbe0301ef73895fa00b71905b9aa
tenpamk2 <wavyroom@gmail.com>
2025-07-13 20:51:22 +0900

    :loud_sound: Colorize logs.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,6 +5,8 @@ mod image_handler;
 mod image_types;
 mod png_handler;
 mod sd_parameters;
+use chrono::Local;
+use colored::*;
 use log;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -15,6 +17,26 @@ pub fn run() {
             tauri_plugin_log::Builder::new()
                 .level(log::LevelFilter::Info)
                 .timezone_strategy(tauri_plugin_log::TimezoneStrategy::UseLocal)
+                .format(|out, message, record| {
+                    let now = Local::now();
+                    let formatted_date_time = now.format("%Y-%m-%d %H:%M:%S").to_string();
+
+                    let colored_level = match record.level() {
+                        log::Level::Error => "ERROR".red(),
+                        log::Level::Warn => "WARN".yellow(),
+                        log::Level::Info => "INFO".green(),
+                        log::Level::Debug => "DEBUG".blue(),
+                        log::Level::Trace => "TRACE".magenta(),
+                    };
+
+                    out.finish(format_args!(
+                        "[{}][{}][{}] {}",
+                        formatted_date_time.cyan(),
+                        colored_level,
+                        record.target().to_string().cyan(),
+                        message
+                    ))
+                })
                 .build(),
         )
         .plugin(tauri_plugin_fs::init())
